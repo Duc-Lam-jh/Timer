1479ba85af9d36e54013da86c044475945780c95
Duc-Lam-jh <duc.lam@journeyh.io>
2021-12-23 11:13:03 +0700

    add question and answer section in UI

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+/* ======== Data ======== */
+const questions = [
+  {
+    question: 'What is the color theme of this page?',
+    answer: 'blue'
+  },
+  {
+    question: 'What is 4 + 4?',
+    answer: '8'
+  },
+  {
+    question: 'What country is Hanoi in?',
+    answer: 'vietnam'
+  },
+  {
+    question: 'Who is the writer of Easy On Me?',
+    answer: 'adele'
+  }
+]
+
+/* ======== Functions ======== */
 function Timer(interval, container){
   const timerContainer = document.getElementById(container);
   const timer = timerContainer.querySelector('#timer');
   let timeElapsed = new Date(0);
   let startingPoint = new Date();
   let start;
-
-  function updateTimer(){
-    timeElapsed.setTime(Date.now() - startingPoint);
-    timer.innerHTML = timeElapsed.toUTCString().substring(20, 25);
-  }
 
   function setUpTimerButtons(){
     const startButton = timerContainer.querySelector('#start-button');
@@ -17,6 +33,11 @@ function Timer(interval, container){
     startButton.addEventListener('click', startTimer);
     stopButton.addEventListener('click', stopTimer);
   }
+
+  function updateTimer(){
+    timeElapsed.setTime(Date.now() - startingPoint);
+    timer.innerHTML = timeElapsed.toUTCString().substring(20, 25);
+  } 
 
   function startTimer() {
     resetTimer();
@@ -38,5 +59,14 @@ function Timer(interval, container){
 
 }
 
+function getRandomQuestion(data){
+  const questionNumber = Math.floor(Math.random() * data.length);
+  document.getElementById('question').innerHTML = data[questionNumber].question;
+}
+
+
+/* ======== Execution ======== */
 const timer = Timer(1000, 'timer-container');
 timer.setUpTimerButtons();
+
+getRandomQuestion(questions);
