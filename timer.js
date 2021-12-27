f6c9c740305708f1f5a3e97f1f8fd27b9b748a26
Duc-Lam-jh <duc.lam@journeyh.io>
2021-12-27 09:22:22 +0700

    fix timer logic in function updateTimer()
    delete public variable elapsedTime and replace with local variable timeElapsedSinceStart to update time

--- timer.js
+++ timer.js
@@ -2,68 +2,74 @@
  * A function to create and initialize a timer starting at 00:00
  * 
  * @param {*} interval interval between each tick of the timer
- * @param {*} options specify the ids of the elemnts that make up the timer: {containerId, timerId, startButtonId, stopButtonId, resetButtonId}
+ * @param {*} options specify the ids of the elements that make up the timer
  * @returns setUpTimerButtons(): a function to set up the buttons of the timer
  */
-function Timer(interval, { }) {
-  const timerContainerId = arguments[1].containerId ? arguments[1].containerId : 'container';
-  const timerId = arguments[1].timerId ? arguments[1].timerId : 'timer';
-  const startButtonId = arguments[1].startButtonId ? arguments[1].startButtonId : 'start-button';
-  const stopButtonId = arguments[1].stopButtonId ? arguments[1].stopButtonId : 'stop-button';
-  const resetButtonId = arguments[1].resetButtonId ? arguments[1].resetButtonId : 'reset-button';
+function Timer(interval, options = { 
+  containerId,
+  timerId,
+  startButtonId,
+  stopButtonId,
+  resetButtonId}) {
+  const timerContainerId = options.containerId ? options.containerId : 'container';
+  const timerId = options.timerId ? options.timerId : 'timer';
+  const startButtonId = options.startButtonId ? options.startButtonId : 'start-button';
+  const stopButtonId = options.stopButtonId ? options.stopButtonId : 'stop-button';
+  const resetButtonId = options.resetButtonId ? options.resetButtonId : 'reset-button';
   
   const timerContainer = document.getElementById(timerContainerId);
   const timer = timerContainer.querySelector('#' + timerId);
 
-  let timeElapsed = new Date(0);
-  let startingPoint = new Date();
-  let start;
+  let timerStartingTime;
+  let isTimerRunning;
 
   const setUpTimerButtons = () => {
-    const startButton = timerContainer.querySelector('#' + startButtonId);
-    startButton.addEventListener('click', startTimer);
+    timerContainer.querySelector('#' + startButtonId).addEventListener('click', startTimer);
     
-    const stopButton = timerContainer.querySelector('#' + stopButtonId);
-    stopButton.addEventListener('click', stopTimer);
+    timerContainer.querySelector('#' + stopButtonId).addEventListener('click', stopTimer);
 
-    const resetButton = timerContainer.querySelector('#' + resetButtonId);
-    resetButton.addEventListener('click', resetTimer);
+    timerContainer.querySelector('#' + resetButtonId).addEventListener('click', resetTimer);
   }
 
+  // const setUpTimerButtons = () => {
+  //   const startButton = timerContainer.querySelector('#' + startButtonId);
+  //   startButton.addEventListener('click', startTimer);
+    
+  //   const stopButton = timerContainer.querySelector('#' + stopButtonId);
+  //   stopButton.addEventListener('click', stopTimer);
+
+  //   const resetButton = timerContainer.querySelector('#' + resetButtonId);
+  //   resetButton.addEventListener('click', resetTimer);
+  // }
+
   const updateTimer = () => {
-    timeElapsed.setTime(Date.now() - startingPoint);
-    timer.innerHTML = timeElapsed.toUTCString().substring(20, 25);
+    const timeElapsedSinceStart = new Date(Date.now() - timerStartingTime);
+    timer.innerHTML = timeElapsedSinceStart.toString().substring(19, 24);
   } 
 
   const startTimer = () => {
-    if(start) {
-      clearInterval(start);
-    } else {
-      //do nothing
+    if(isTimerRunning) {
+      clearInterval(isTimerRunning);
     }
     resetTimer();
-    start = setInterval(updateTimer, interval);
+    isTimerRunning = setInterval(updateTimer, interval);
   }
 
   const stopTimer = () => {
-    if(start) {
-      clearInterval(start);
-    } else {
-      //do nothing
+    if(isTimerRunning) {
+      clearInterval(isTimerRunning);
     }
   }
 
   const resetTimer = () => {
     stopTimer();
-    timeElapsed = new Date(0);
-    startingPoint = new Date();
+    timerStartingTime = new Date();
     timer.innerHTML = '00:00';
   }
 
   return{
     setUpTimerButtons,
   }
-
 }
 
 export default Timer;
